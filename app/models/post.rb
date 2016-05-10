2b9ef92bb2d11da960fd73c4118b5400e3fdee50
Adrian Vogeltanz <avogeltanz@Adrians-iMac.attlocal.net>
2016-05-10 18:50:27 -0500

    satisfied validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  validates_presence_of :title, :description
+  validates_length_of :description, within: 5..40, too_long: 'pick a shorter description', too_short: 'pick a longer description'
 end
